941de5d9cf1e8eb9f8e1a693043b25b437fa354c
Aravindan Venkataraju <aravindan.venkataraju@gmail.com>
2019-02-26 18:24:18 +1100

    initial-version

---
+++ src/main/java/com/aravindan/springframework/joke/SpringbootJokeAppApplication.java
@@ -0,0 +1,13 @@
+package com.aravindan.springframework.joke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJokeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJokeAppApplication.class, args);
+	}
+
+}
