439b4cd8202b891868d23ef7a9097eb7232aee7d
Aravindan Venkataraju <aravindan.venkataraju@gmail.com>
2019-02-26 22:35:59 +1100

    use-xml-configuration-to-get-bean

--- src/main/java/com/aravindan/springframework/joke/SpringbootJokeAppApplication.java
+++ src/main/java/com/aravindan/springframework/joke/SpringbootJokeAppApplication.java
@@ -2,10 +2,12 @@ package com.aravindan.springframework.joke;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 
 @SpringBootApplication
+@ImportResource("classpath:springboot-joke-app-config.xml")
 public class SpringbootJokeAppApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/aravindan/springframework/joke/services/JokeServiceImpl.java
+++ src/main/java/com/aravindan/springframework/joke/services/JokeServiceImpl.java
@@ -1,6 +1,5 @@
 package com.aravindan.springframework.joke.services;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
